f38f86f7976cb211b1fa263e1a6d902967200745
Luis Gusstavo Fé Varella <lgfvarella@gmail.com>
2022-01-25 13:46:24 -0300

    Update ex0068.py
    Concluí o exercício 68

--- exercicios/ex0068.py
+++ exercicios/ex0068.py
@@ -1,10 +1,25 @@
 #Exercício Python 68: Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 
 from random import randint
+contador = 0
 print('-'*30)
-print('Sou seu computador!! Vamos jogar PAR ou IMPAR?')
-
-print('Escolha um número: ')
-
-nao esquece o esquema de dividir para achar o resto
-
+print('Sou seu computador!! Vamos jogar PAR ou IMPAR!!')
+print('-'*30)
+while True:
+    numero_jogador = int(input('Escolha um número: '))
+    numero_pc = (randint(0,10))
+    escolha_jogador =(str(input('Você quer par ou impar? [P/I]'))).upper() .strip() [0]
+    soma = numero_jogador + numero_pc
+    if soma % 2 == 0:
+        resultado = 'Par'
+    else:
+        resultado = 'Impar'
+    if resultado [0] == escolha_jogador:
+        contador +=1 
+        print(f'A soma entre os numeros escolhidos foi {soma}. Esse é um número {resultado}')
+        print(f'Você ganhou!!')
+    else:
+        print(f'A soma entre os numeros escolhidos foi {soma}. Esse é um número {resultado}')
+        print(f'Você perdeu após {contador} vitórias consecutivas!!')
+        break
+print('FIM')
