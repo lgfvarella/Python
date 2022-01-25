a933d64207dac643eab9f7f922c4ba01c638e37c
Luis Gusstavo Fé Varella <lgfvarella@gmail.com>
2022-01-25 11:00:06 -0300

    add ex0068.py
     programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.

---
+++ exercicios/ex0068.py
@@ -0,0 +1,10 @@
+#Exercício Python 68: Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+
+from random import randint
+print('-'*30)
+print('Sou seu computador!! Vamos jogar PAR ou IMPAR?')
+
+print('Escolha um número: ')
+
+nao esquece o esquema de dividir para achar o resto
+
