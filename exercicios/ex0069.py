35e638d364c74fd8931a3cab3ce6ed7256a09306
Luis Gusstavo Fé Varella <lgfvarella@gmail.com>
2022-01-27 22:51:19 -0300

    Update ex0069.py
    Coloquei as validações para inserção de dados.

--- exercicios/ex0069.py
+++ exercicios/ex0069.py
@@ -8,20 +8,23 @@ print('-'*30)
 de_maior = novinhas = homens = 0
 while True:
     idade = 0
+    sexo = encerrar = ' '
     while idade not in range(1,200):
         idade = int(input('Digite sua idade: '))
     if idade > 18:
         de_maior += 1
-    sexo = str(input('Qual o seu sexo? [M/F]: ')).strip() .upper() [0]
+    while sexo not in ('MmFf'):
+        sexo = str(input('Qual o seu sexo? [M/F]: ')).strip() .upper() [0]
     if sexo == 'F' and idade < 20:
         novinhas += 1
     if sexo == 'M':
         homens += 1
-    encerrar = str(input('Deseja encerrar o programa? [Y/N]')).strip() .upper() [0]
+    while encerrar not in ('YyNn'):
+        encerrar = str(input('Deseja encerrar o programa? [Y/N]')).strip() .upper() [0]
     if encerrar == 'Y':
         break
-print (f'Temos {de_maior} pessoas com mais de 18 anos.')
-print (f'Temos {novinhas} mulheres com menos de 20 anos.')
-print (f'{homens} homens foram cadastrados')
+print (f'Temos \033[01;34;40m{de_maior}\033[m pessoa(s) com mais de 18 anos.')
+print (f'Temos \033[01;34;40m{novinhas}\033[m mulher(es) com menos de 20 anos.')
+print (f'\033[01;34;40m{homens}\033[m homem(s) foram cadastrados')
 print ('FIM')
 print ('-'*30)
